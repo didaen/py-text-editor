e687d7d78d375b2ba1e32a62c005fa276a7e1191
Dida En <firdanb@students.unnes.ac.id>
2023-05-25 10:50:27 +0700

    Menambahkan <link> untuk FEATHER ICON pada <head> index.php.

--- index.php
+++ index.php
@@ -4,10 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- MY STYLE -->
     <link rel="stylesheet" href="style.css">
+
+    <!-- FEATHER ICON -->
+    <script src="https://unpkg.com/feather-icons"></script>
+    
     <title>Input Text</title>
 </head>
 <body>
-    
+    <div>
+
+    </div>
 </body>
 </html>
