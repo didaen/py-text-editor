e0abc4597c346f5698870b1714a6aeb0dc35f751
Dida En <firdanb@students.unnes.ac.id>
2023-05-25 11:23:21 +0700

    Berhasil menambahkan icon dari feather icon dan memunculkan icon.

--- index.php
+++ index.php
@@ -4,18 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    
     <!-- MY STYLE -->
     <link rel="stylesheet" href="style.css">
-
+    
     <!-- FEATHER ICON -->
     <script src="https://unpkg.com/feather-icons"></script>
     
     <title>Input Text</title>
 </head>
 <body>
-    <div>
+    <div class="add-input" data-feather="edit" class="align-text-bottom"></div>
 
-    </div>
+    <script>
+        feather.replace()
+    </script>
 </body>
 </html>
