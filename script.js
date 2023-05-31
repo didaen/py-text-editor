71c93aeac321bb208ea2b8eba797f5b89cfd786e
Dida En <firdanb@students.unnes.ac.id>
2023-05-31 15:42:55 +0700

    Berhasil menjalankan function addInput() untuk memunculkan dan menyembunyikan tooltip serta mengganti warna dan fill dari feather icon.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const addInputLight = document.querySelector("#add-input-light");
+const tooltipText = document.querySelector(".tooltiptext");
+tooltipText.style.visibility = "hidden";
+const addInputLight = document.getElementById("add-input-light");
+
+function addInput() {
+    if (tooltipText.style.visibility == "hidden") {
+        tooltipText.style.visibility = "visible";
+        addInputLight.setAttribute("color", "red");
+        addInputLight.setAttribute("fill", "green");
+    } else {
+        tooltipText.style.visibility = "hidden";
+        addInputLight.setAttribute("color", "");
+        addInputLight.setAttribute("fill", "white");
+    }
+}
+
+// JADI KAN FUNCTIONNYA SUDAH DIJALANKAN KETIKA HALAMAN PERTAMA KALI LOAD JADI GAK AKAN DIJALANIN LAGI KECUALI DIPAKEIN EVENT LISTENER
